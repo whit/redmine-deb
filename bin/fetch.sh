0726cb994acab5ac2191a3537970438e65276091
yac <yac@blesmrt.net>
2012-02-20 17:53:38 +0100

    + basic data for package to build
    *	bin/fetch script
    *	rules
    *	control
    *	changelog
    *	install

---
+++ bin/fetch.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env sh
+
+set -x
+set -eu
+
+archive=redmine-1.2.2.tar.gz
+
+fetch_cache() {
+	CACHE_DIR=${CACHE_DIR:-$HOME/redmine-dev-cache}
+
+	test -d $CACHE_DIR && test -f $CACHE_DIR/$archive && cp $CACHE_DIR/$archive ./
+}
+
+fetch_website() {
+	url=http://rubyforge.org/frs/download.php/75518/$archive
+	wget $url
+}
+
+main() {
+	mkdir build
+	cd build
+	fetch_cache || fetch_website
+	tar -zxf $archive
+}
+
+main
